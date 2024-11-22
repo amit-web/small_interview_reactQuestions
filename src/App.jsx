0cd0167c63db46af9701e1438c7180ab49b2d182
amit-web <mehtaamit9334@gmail.com>
2024-11-22 14:05:16 +0530

    small update

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import "./App.css";
-import Corousel from "./components/corousel/Corousel";
-import DynamicCheckbox from "./components/DynamicCheckboxCounter/DynamicCheckbox";
-import InfiniteVirtualizedList from "./components/VirtualizedList/InfiniteVirtualizedList";
-//import VirtualizedList from "./components/VirtualizedList/VirtualizedList";
+   import "./App.css";
+// import Corousel from "./components/corousel/Corousel";
+// import DynamicCheckbox from "./components/DynamicCheckboxCounter/DynamicCheckbox";
+// import InfiniteVirtualizedList from "./components/VirtualizedList/InfiniteVirtualizedList";
+// import VirtualizedList from "./components/VirtualizedList/VirtualizedList";
 // import AboutUs from "./components/tabNavigation/AboutUs";
 // import Contact from "./components/tabNavigation/Contact";
 // import Home from "./components/tabNavigation/Home";
 // import Register from "./components/tabNavigation/Register";
 // import DropdownMenu from "./components/Dropdown/DropdownMenu";
 // import SignUp from "./components/formvalidation/SignUp";
-// import Memoization from "./components/MemoizationExample/Memoization";
-//import RoutingExample from "./components/tabNavigation/RoutingExample";
+import Memoization from "./components/MemoizationExample/Memoization";
+// import RoutingExample from "./components/tabNavigation/RoutingExample";
 // import MainModal from './components/modlComponent/MainModal';
 // import Modal from "./components/modlComponent/Modal";
-//import DebauncedSearch from "./components/DebauncedSearchBar/DebauncedSearch";
+// import DebauncedSearch from "./components/DebauncedSearchBar/DebauncedSearch";
 // import Counter from "./components/counter/Counter";
 // import FetcchApi from "./components/FetchDataAPI/FetcchApi";
-// import Todomain from "./components/Todomain";
-
+//import Todomain from "./components/TodoApplication/Todomain"
 // import {createBrowserRouter,RouterProvider}  from "react-router-dom"
 function App() {
   // const router = createBrowserRouter([
@@ -58,12 +57,11 @@ function App() {
         {/* <Memoization/> */}
         {/* <RouterProvider router={router}>
             <RoutingExample/>
-        </RouterProvider> */}
-        {/*         
-        <Corousel/> */}
+        </RouterProvider> */}          
+        {/* <Corousel/> */}
         {/* <DynamicCheckbox /> */}
         {/* <VirtualizedList/> */}
-        <InfiniteVirtualizedList/>
+        {/* <InfiniteVirtualizedList/> */}
       </div>
     </>
   );
--- src/components/formvalidation/SignUp.jsx
+++ src/components/formvalidation/SignUp.jsx
@@ -4,7 +4,6 @@
 
 
 import React, { useState } from "react";
-
 const SignUp = () => {
     const [firstName, setFirstName] = useState("");
     const [lastName, setLastName] = useState("");
